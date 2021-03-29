10f58f2bc0724fabf5b8068004631776e5eeaba5
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-03-29 15:55:16 +0900

    ADD : Rx 라이브러리 추가, FIX : API 통신 완료, CoronaInfo파일 수정

--- app/src/main/java/kr/hs/dgsw/where_corona/data/retrofit_builder/API.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/data/retrofit_builder/API.kt
@@ -1,5 +1,11 @@
 package kr.hs.dgsw.where_corona.data.retrofit_builder
 
-interface API {
+import io.reactivex.Single
+import kr.hs.dgsw.where_corona.data.model.response_model.CoronaInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface API {
+    @GET(".")
+    fun getCoronaInfo(@Query("serviceKey")serviceKey: String): Single<CoronaInfo>
 }
