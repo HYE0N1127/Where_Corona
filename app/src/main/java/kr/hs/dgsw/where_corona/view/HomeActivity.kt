327a96e39554e7cb6f6d5ceb0c3e451baee30e1d
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-04-19 10:58:42 +0900

    [UPDATE] Update HomeActivity

---
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/HomeActivity.kt
@@ -0,0 +1,26 @@
+package kr.hs.dgsw.where_corona.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.hs.dgsw.where_corona.R
+import kr.hs.dgsw.where_corona.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding =
+            DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
+
+        binding.btnCityStatus.setOnClickListener {
+            startActivity(Intent(this, CityStatusActivity::class.java))
+        }
+        binding.btnTodayStatus.setOnClickListener {
+            startActivity(Intent(this, TodayStatusActivity::class.java))
+        }
+        binding.btnKoreaStatus.setOnClickListener {
+            startActivity(Intent(this, KoreaStatusActivity::class.java))
+        }
+    }
+}
