3c45f2a4902d15de0c8805a3b06e86d26b083636
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-04-19 10:56:54 +0900

    [ADD] Add Views

---
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/CityStatusActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.where_corona.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.where_corona.R
+
+class CityStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_status)
+    }
+}
