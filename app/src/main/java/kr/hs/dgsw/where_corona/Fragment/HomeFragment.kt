cc01b8252678e050435b03983cb1a74072804de4
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-05-31 20:26:26 +0900

    ADD : Navigation Component 파일 추가

---
+++ app/src/main/java/kr/hs/dgsw/where_corona/Fragment/HomeFragment.kt
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.where_corona.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.hs.dgsw.where_corona.R
+
+class HomeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
