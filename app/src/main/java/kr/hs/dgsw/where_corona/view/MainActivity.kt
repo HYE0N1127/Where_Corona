cf313f32532c8f88a553a63402e039c5080bbb3e
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-05-31 10:46:41 +0900

    ADD : Gradle

--- app/src/main/java/kr/hs/dgsw/where_corona/view/MainActivity.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/MainActivity.kt
@@ -2,29 +2,11 @@ package kr.hs.dgsw.where_corona.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kr.hs.dgsw.where_corona.R
-import kr.hs.dgsw.where_corona.data.retrofit_builder.RetrofitBuilder
-import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val d = CompositeDisposable()
-        d.add(
-        RetrofitBuilder.api.getCoronaInfo("NRIrT3EzAtL4Oc7QkYmDuH58M2JPn6xBy")
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({
-                    Log.d("TEST", "Success")
-                },{
-                    Log.d("TEST", "${it.message}")
-                    Log.d("TEST", "Fail")
-                }))
     }
 }
