e481ebb8ea5acb7c211e91e8577f390084dfe06c
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-06-12 18:30:59 +0900

    FIX : 파일 구조 변경

--- app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/HomeFragment.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/HomeFragment.kt
@@ -1,7 +1,6 @@
-package kr.hs.dgsw.where_corona.Fragment
+package kr.hs.dgsw.where_corona.view.activity.view.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/KoreaStatusFragment.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/KoreaStatusFragment.kt
@@ -1,4 +1,4 @@
-package kr.hs.dgsw.where_corona.Fragment
+package kr.hs.dgsw.where_corona.view.activity.view.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import kr.hs.dgsw.where_corona.R
 
-class TodayStatusFragment : Fragment() {
+class KoreaStatusFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_status, container, false)
+        return inflater.inflate(R.layout.fragment_korea_status, container, false)
     }
 }
