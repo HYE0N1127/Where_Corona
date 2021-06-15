1aa36815eb6a15282ee41c6c4c82e65deaef54d1
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-06-15 18:11:53 +0900

    ADD : BaseFragment, ViewModel 파일 추가

--- app/src/main/java/kr/hs/dgsw/where_corona/base/fragment/BaseFragment.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/base/fragment/BaseFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import kr.hs.dgsw.where_corona.BR
+import kr.hs.dgsw.where_corona.R
 import kr.hs.dgsw.where_corona.base.viewmodel.BaseViewModel
+import java.lang.StringBuilder
+import java.lang.reflect.ParameterizedType
+import java.util.*
 
 abstract class  BaseFragment<VB : ViewDataBinding, VM : BaseViewModel> : Fragment() {
     protected lateinit var mBinding: VB
@@ -36,7 +42,42 @@ abstract class  BaseFragment<VB : ViewDataBinding, VM : BaseViewModel> : Fragmen
         performDataBinding()
 
         mViewModel.errorEvent.observe(viewLifecycleOwner, { onErrorEvent(it) })
-        
+        observerViewModel()
+    }
+
+    private fun performDataBinding() {
+        mViewModel = if(::mViewModel.isInitialized) mViewModel else viewModel
+        mBinding.setVariable(BR.vm, mViewModel)
+        mBinding.lifecycleOwner = this
+        mBinding.executePendingBindings()
+    }
+
+    @LayoutRes
+    private fun layoutRes(): Int {
+        val split =
+            ((Objects.requireNonNull(javaClass.genericSuperclass) as ParameterizedType).actualTypeArguments[0] as Class<*>)
+                .simpleName.replace("Binding$".toRegex(), "")
+                .split("(?<=.)(?=\\p{Upper})".toRegex())
+                .dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        val name = StringBuilder()
+
+        for(i in split.indices) {
+            name.append(split[i].toLowerCase(Locale.ROOT))
+
+            if (i != split.size - 1)
+                name.append("_")
+        }
+
+        try {
+            return R.layout::class.java.getField(name.toString()).getInt(R.layout::class.java)
+        }catch (e : IllegalAccessException) {
+            e.printStackTrace()
+        } catch (e : NoSuchFieldError) {
+            e.printStackTrace()
+        }
+
+        return 0
     }
 
 }
---
+++ app/src/main/java/kr/hs/dgsw/where_corona/viewmodel/HomeViewModel.kt
@@ -0,0 +1,4 @@
+package kr.hs.dgsw.where_corona.viewmodel
+
+class HomeViewModel {
+}
