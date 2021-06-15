58a69650ffd9beab93df8fadff5cfdac7967d26a
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-06-15 20:01:13 +0900

    ADD : HomeViewModel

--- app/src/main/java/kr/hs/dgsw/where_corona/viewmodel/HomeViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/viewmodel/HomeViewModel.kt
@@ -1,4 +1,29 @@
 package kr.hs.dgsw.where_corona.viewmodel
 
-class HomeViewModel {
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.Completable
+import io.reactivex.observers.DisposableCompletableObserver
+import kr.hs.dgsw.where_corona.base.viewmodel.BaseViewModel
+
+class HomeViewModel : BaseViewModel() {
+    val onWhereImageEvent = MutableLiveData<Unit>()
+    val onTodayRecordEvent = MutableLiveData<Unit>()
+    val onKoreaRecordEvent = MutableLiveData<Unit>()
+    val onCityRecordEvent = MutableLiveData<Unit>()
+
+    fun whereImageEvent() {
+        onWhereImageEvent.value = Unit
+    }
+
+    fun todayRecordEvent() {
+        onTodayRecordEvent.value = Unit
+    }
+
+    fun koreaRecordEvent() {
+        onKoreaRecordEvent.value = Unit
+    }
+
+    fun cityRecordEvent() {
+        onCityRecordEvent.value = Unit
+    }
 }
