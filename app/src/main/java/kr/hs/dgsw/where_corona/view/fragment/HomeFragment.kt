cdc9009553fe1db9ac40262690ab006d99c45ed1
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-06-16 18:13:34 +0900

    ADD : WhereImageDialog 이벤트

--- app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/HomeFragment.kt
+++ app/src/main/java/kr/hs/dgsw/where_corona/view/fragment/HomeFragment.kt
@@ -7,36 +7,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import kr.hs.dgsw.where_corona.R
 import kr.hs.dgsw.where_corona.base.fragment.BaseFragment
 import kr.hs.dgsw.where_corona.databinding.FragmentHomeBinding
+import kr.hs.dgsw.where_corona.view.dialog.WhereImageFragment
 import kr.hs.dgsw.where_corona.viewmodel.HomeViewModel
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override val viewModel: HomeViewModel by viewModels()
 
     override fun observerViewModel() {
-        with(viewModel) {
+        with(mViewModel) {
+            val whereImageDialog = WhereImageFragment()
+
             onWhereImageEvent.observe(this@HomeFragment, {
                 Log.d("TEST", "이미지 출처표기 버튼이 클릭되었습니다.")
+                if(!whereImageDialog.isAdded)
+                    whereImageDialog.show(activity?.supportFragmentManager!!, whereImageDialog.tag)
             })
 
             onCityRecordEvent.observe(this@HomeFragment, {
-                Log.d("TEST", "도시별 확진자 기록 체크 버튼이 클릭되었습니다.")
                 view?.findNavController()?.navigate(R.id.action_homeFragment_to_cityStatusFragment)
             })
 
             onKoreaRecordEvent.observe(this@HomeFragment, {
-                Log.d("TEST", "한국 확진자 기록 확인 버튼이 클릭되었습니다.")
                 view?.findNavController()?.navigate(R.id.action_homeFragment_to_koreaStatusFragment)
             })
 
             onTodayRecordEvent.observe(this@HomeFragment, {
-                Log.d("TEST", "당일 확진자 기록 확인 버튼이 클릭되었습니다.")
                 view?.findNavController()?.navigate(R.id.action_homeFragment_to_todayStatusFragment)
+//                mBinding.root?.findNavController()?.navigate(R.id.action_homeFragment_to_todayStatusFragment)
+//                위의 코드는 가능하다는 것만 알려주는 코드
             })
         }
     }
